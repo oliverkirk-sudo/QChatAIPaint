314418f02b4f7a76826f3da9db1877628307a1f7
wenyu song <609923924@qq.com>
2023-08-29 18:06:50 +0800

    修改函数名

--- main.py
+++ main.py
@@ -11,114 +11,106 @@ from mirai import Image, Plain
 
 def response_process(images, remain, generation_cost):
     msg_list = []
-    img_obj = [Image(base64=i.replace("\n", "")) for i in images]
+    img_obj = [Image(base64=i.replace('\n', '')) for i in images]
     msg_list += img_obj
-    msg_list.append(Plain(text="剩余积分：{}\n".format(remain)))
-    msg_list.append(Plain(text="使用成本：{}".format(generation_cost)))
+    msg_list.append(Plain(text='剩余积分：{}\n'.format(remain)))
+    msg_list.append(Plain(text='使用成本：{}'.format(generation_cost)))
     return msg_list
 
 
 def default_random_gen():
-    return gen_img(
-        True,
-        "",
-        default_config["negative_prompt"],
-        default_config["model"],
-        default_config["width"],
-        default_config["height"],
-        default_config["steps"],
-        default_config["cfg_scale"],
-        default_config["strength"],
-        str(random.randint(10**8, 10**10 - 1)),
-        default_config["history"],
-        default_config["random_autogen"],
-        default_config["variations_autogen"],
-        default_config["quality_tags"],
-        default_config["num_images"],
-        "",
-        default_config["skip"],
-        default_config["continuous_mode"],
-    )
+    return gen_img(True,
+                   '',
+                   default_config['negative_prompt'],
+                   default_config['model'],
+                   default_config['width'],
+                   default_config['height'],
+                   default_config['steps'],
+                   default_config['cfg_scale'],
+                   default_config['strength'],
+                   str(random.randint(10 ** 8, 10 ** 10 - 1)),
+                   default_config['history'],
+                   default_config['random_autogen'],
+                   default_config['variations_autogen'],
+                   default_config['quality_tags'],
+                   default_config['num_images'],
+                   '',
+                   default_config['skip'],
+                   default_config['continuous_mode'])
 
 
 def condition_random_gen(con_list):
-    return gen_img(
-        True,
-        "",
-        default_config["negative_prompt"],
-        con_list[0],
-        con_list[1] if default_config["user_resize"] else default_config["width"],
-        con_list[2] if default_config["user_resize"] else default_config["height"],
-        con_list[3],
-        con_list[4],
-        default_config["strength"],
-        str(random.randint(10**8, 10**10 - 1)),
-        default_config["history"],
-        default_config["random_autogen"],
-        default_config["variations_autogen"],
-        default_config["quality_tags"],
-        con_list[5] if not default_config["gen_limit"] else "1",
-        "",
-        default_config["skip"],
-        default_config["continuous_mode"],
-    )
+    return gen_img(True,
+                   '',
+                   default_config['negative_prompt'],
+                   con_list[0],
+                   con_list[1] if default_config['user_resize'] else default_config['width'],
+                   con_list[2] if default_config['user_resize'] else default_config['height'],
+                   con_list[3],
+                   con_list[4],
+                   default_config['strength'],
+                   str(random.randint(10 ** 8, 10 ** 10 - 1)),
+                   default_config['history'],
+                   default_config['random_autogen'],
+                   default_config['variations_autogen'],
+                   default_config['quality_tags'],
+                   con_list[5] if not default_config['gen_limit'] else '1',
+                   '',
+                   default_config['skip'],
+                   default_config['continuous_mode'])
 
 
 def condition_gen(con_list):
-    return gen_img(
-        False,
-        con_list[0],
-        default_config["negative_prompt"] + con_list[1],
-        con_list[2],
-        con_list[3] if default_config["user_resize"] else default_config["width"],
-        con_list[4] if default_config["user_resize"] else default_config["height"],
-        con_list[5],
-        con_list[6],
-        default_config["strength"],
-        str(random.randint(10**8, 10**10 - 1)),
-        default_config["history"],
-        default_config["random_autogen"],
-        default_config["variations_autogen"],
-        default_config["quality_tags"],
-        con_list[7] if not default_config["gen_limit"] else "1",
-        "",
-        default_config["skip"],
-        default_config["continuous_mode"],
-    )
+    return gen_img(False,
+                   con_list[0],
+                   default_config['negative_prompt'] + con_list[1],
+                   con_list[2],
+                   con_list[3] if default_config['user_resize'] else default_config['width'],
+                   con_list[4] if default_config['user_resize'] else default_config['height'],
+                   con_list[5],
+                   con_list[6],
+                   default_config['strength'],
+                   str(random.randint(10 ** 8, 10 ** 10 - 1)),
+                   default_config['history'],
+                   default_config['random_autogen'],
+                   default_config['variations_autogen'],
+                   default_config['quality_tags'],
+                   con_list[7] if not default_config['gen_limit'] else '1',
+                   '',
+                   default_config['skip'],
+                   default_config['continuous_mode'])
 
 
 def default_condition_gen(con_list):
-    return gen_img(
-        False,
-        con_list[0],
-        default_config["negative_prompt"] + con_list[1],
-        default_config["model"],
-        default_config["width"],
-        default_config["height"],
-        default_config["steps"],
-        default_config["cfg_scale"],
-        default_config["strength"],
-        str(random.randint(10**8, 10**10 - 1)),
-        default_config["history"],
-        default_config["random_autogen"],
-        default_config["variations_autogen"],
-        default_config["quality_tags"],
-        default_config["num_images"],
-        "",
-        default_config["skip"],
-        default_config["continuous_mode"],
-    )
+    return gen_img(False,
+                   con_list[0],
+                   default_config['negative_prompt'] + con_list[1],
+                   default_config['model'],
+                   default_config['width'],
+                   default_config['height'],
+                   default_config['steps'],
+                   default_config['cfg_scale'],
+                   default_config['strength'],
+                   str(random.randint(10 ** 8, 10 ** 10 - 1)),
+                   default_config['history'],
+                   default_config['random_autogen'],
+                   default_config['variations_autogen'],
+                   default_config['quality_tags'],
+                   default_config['num_images'],
+                   '',
+                   default_config['skip'],
+                   default_config['continuous_mode'])
 
 
 def send_msg(kwargs, msg):
-    host: pkg.plugin.host.PluginHost = kwargs["host"]
-    host.send_person_message(kwargs["launcher_id"], msg) if kwargs[
-        "launcher_type"
-    ] == "person" else host.send_group_message(kwargs["launcher_id"], msg)
+    host: pkg.plugin.host.PluginHost = kwargs['host']
+    host.send_person_message(kwargs['launcher_id'], msg) if kwargs[
+                                                                'launcher_type'] == 'person' else host.send_group_message(
+        kwargs['launcher_id'], msg)
 
 
 # 注册插件
-@register(name="AIPaint", description="ai绘画", version="0.1", author="oliverkirk")
+@register(name="QChatAIPaint", description="ai绘画", version="0.2", author="oliverkirk-sudo")
 class HelloPlugin(Plugin):
 
     # 插件加载时触发
@@ -130,114 +122,100 @@ class HelloPlugin(Plugin):
     @on(PersonNormalMessageReceived)
     @on(GroupNormalMessageReceived)
     def normal_message_received(self, event: EventContext, **kwargs):
-        msg: str = kwargs["text_message"].strip()
-        if msg.startswith("QChatAIPaint"):
-            if not default_config["open"]:
-                event.add_return("reply", ["ai绘画功能未开启"])
-            elif default_config["sessionid"] == "" or default_config["holara_r"] == "":
+        msg: str = kwargs['text_message'].strip()
+        if msg.startswith('aipaint'):
+            if not default_config['open']:
+                event.add_return("reply", ['ai绘画功能未开启'])
+            elif default_config['sessionid'] == '' or default_config['holara_r'] == '':
                 event.add_return("reply", ["请设置sessionid与holara_r"])
             else:
-                logging.debug("{}使用了ai绘画工具".format(kwargs["sender_id"]))
-                if msg.replace("QChatAIPaint", "").strip() in ["随机图片", "随机生成", "随机"]:
-                    send_msg(kwargs, ["图片生成中，请稍候"])
+                logging.debug("{}使用了ai绘画工具".format(kwargs['sender_id']))
+                if msg.replace('aipaint', '').strip() in ['随机图片', '随机生成', '随机']:
+                    send_msg(kwargs, ['图片生成中，请稍候'])
                     images, remain, generation_cost = default_random_gen()
                     msg_list = response_process(images, remain, generation_cost)
-                    logging.debug("{}使用了随机图片生成".format(kwargs["sender_id"]))
-                elif "条件随机" in msg.replace("QChatAIPaint", "").strip():
-                    con_list = (
-                        msg.replace("QChatAIPaint", "")
-                        .replace("条件随机", "")
-                        .strip()
-                        .split("?")
-                    )
-                    send_msg(kwargs, ["图片生成中，请稍候"])
+                    logging.debug("{}使用了随机图片生成".format(kwargs['sender_id']))
+                elif '条件随机' in msg.replace('aipaint', '').strip():
+                    con_list = msg.replace('aipaint', '').replace('条件随机', '').strip().split('?')
+                    send_msg(kwargs, ['图片生成中，请稍候'])
                     images, remain, generation_cost = condition_random_gen(con_list)
                     msg_list = response_process(images, remain, generation_cost)
-                    logging.debug("{}使用了条件随机生成".format(kwargs["sender_id"]))
-                elif "默认生成" in msg.replace("QChatAIPaint", "").strip():
-                    con_list = (
-                        msg.replace("QChatAIPaint", "")
-                        .replace("默认生成", "")
-                        .strip()
-                        .split("?")
-                    )
-                    send_msg(kwargs, ["图片生成中，请稍候"])
+                    logging.debug("{}使用了条件随机生成".format(kwargs['sender_id']))
+                elif '默认生成' in msg.replace('aipaint', '').strip():
+                    con_list = msg.replace('aipaint', '').replace('默认生成', '').strip().split('?')
+                    send_msg(kwargs, ['图片生成中，请稍候'])
                     images, remain, generation_cost = default_condition_gen(con_list)
                     msg_list = response_process(images, remain, generation_cost)
-                    logging.debug("{}使用了默认配置图片生成".format(kwargs["sender_id"]))
-                elif "条件生成" in msg.replace("QChatAIPaint", "").strip():
-                    con_list = (
-                        msg.replace("QChatAIPaint", "")
-                        .replace("条件生成", "")
-                        .strip()
-                        .split("?")
-                    )
-                    send_msg(kwargs, ["图片生成中，请稍候"])
+                    logging.debug("{}使用了默认配置图片生成".format(kwargs['sender_id']))
+                elif '条件生成' in msg.replace('aipaint', '').strip():
+                    con_list = msg.replace('aipaint', '').replace('条件生成', '').strip().split('?')
+                    send_msg(kwargs, ['图片生成中，请稍候'])
                     images, remain, generation_cost = condition_gen(con_list)
                     msg_list = response_process(images, remain, generation_cost)
-                    logging.debug("{}使用了条件图片生成".format(kwargs["sender_id"]))
+                    logging.debug("{}使用了条件图片生成".format(kwargs['sender_id']))
                 else:
                     msg_list = ["意外参数"]
-            event.add_return("reply", msg_list)
+            # event.add_return("reply", msg_list)
+            send_msg(kwargs, msg_list)
             event.prevent_default()
             event.prevent_postorder()
 
     @on(PersonCommandSent)
     @on(GroupCommandSent)
-    def normal_message_received(self, event: EventContext, **kwargs):
-        command = kwargs["command"]
-        params = kwargs["params"]
-        if command == "QChatAIPaint":
-            if params[0] == "help":
-                help_str = """欢迎使用ai绘图工具，本工具基于https://holara.ai/网站逆向爬虫\n绘图功能分为：\n1. 随机图片生成：【aipaint随机图片】或【aipaint随机】或【aipaint随机生成】\n2. 条件随机图片生成：【aipaint条件随机model?width?height?steps?cfg_scale?num_images】\n3. 默认配置条件生成：【aipaint默认生成tag?ntag】\n4. 条件图片生成：【aipaint条件生成tag?ntag?model?width?height?steps?cfg_scale?num_images】\n其中，model包含：\n1、Vibrance #Vibrant and bright\n2、Chroma #Inspired by ghostmix/AOM3\n3、Aika #Holara's aesthetic model\n4、Tranquility #Inspired by counterfeit3\n5、Lucid #Realistic and smooth\n6、Furry #Anthropomorphic furry model\n7、Akasha #Holara's traditional model\n8、Yami #Holara's alternative model\n管理员功能为：\n!QChatAIPaint on 开启ai绘画功能\n!QChatAIPaint off 关闭ai绘画功能\n!QChatAIPaint set user_resize <true/false> 是否允许用户设置图片大小\n!QChatAIPaint set gen_limit <true/false> 是否开启生成数量限制\n#该软件仅供学习交流，一切由该软件产生的不良影响由使用者承担#\n最后更新日期：2023年8月10日"""
+    def command_message_received(self, event: EventContext, **kwargs):
+        command = kwargs['command']
+        params = kwargs['params']
+        if command == 'aipaint':
+            if params[0] == 'help':
+                help_str = '''欢迎使用ai绘图工具，本工具基于https://holara.ai/网站逆向爬虫\n绘图功能分为：\n1. 随机图片生成：【aipaint随机图片】或【aipaint随机】或【aipaint随机生成】\n2. 条件随机图片生成：【aipaint条件随机model?width?height?steps?cfg_scale?num_images】\n3. 默认配置条件生成：【aipaint默认生成tag?ntag】\n4. 条件图片生成：【aipaint条件生成tag?ntag?model?width?height?steps?cfg_scale?num_images】\n其中，model包含：\n1、Vibrance #Vibrant and bright\n2、Chroma #Inspired by ghostmix/AOM3\n3、Aika #Holara's aesthetic model\n4、Tranquility #Inspired by counterfeit3\n5、Lucid #Realistic and smooth\n6、Furry #Anthropomorphic furry model\n7、Akasha #Holara's traditional model\n8、Yami #Holara's alternative model\n管理员功能为：\n!aipaint on 开启ai绘画功能\n!aipaint off 关闭ai绘画功能\n!aipaint set user_resize <true/false> 是否允许用户设置图片大小\n!aipaint set gen_limit <true/false> 是否开启生成数量限制\n#该软件仅供学习交流，一切由该软件产生的不良影响由使用者承担#\n最后更新日期：2023年8月10日'''
                 event.add_return("reply", [help_str])
-            if kwargs["is_admin"]:
-                if params[0] == "on":
-                    default_config["open"] = True
+            if kwargs['is_admin']:
+                if params[0] == 'on':
+                    default_config['open'] = True
                     event.add_return("reply", ["ai绘画已开启"])
-                    logging.debug("{}ai绘画已开启".format(kwargs["sender_id"]))
-                elif params[0] == "off":
-                    default_config["open"] = False
+                    logging.debug("{}ai绘画已开启".format(kwargs['sender_id']))
+                elif params[0] == 'off':
+                    default_config['open'] = False
                     event.add_return("reply", ["ai绘画已关闭"])
-                    logging.debug("{}ai绘画已关闭".format(kwargs["sender_id"]))
-                elif params[0] == "set":
-                    if params[1] == "user_resize":
-                        if params[2] == "true":
-                            default_config["user_resize"] = True
+                    logging.debug("{}ai绘画已关闭".format(kwargs['sender_id']))
+                elif params[0] == 'set':
+                    if params[1] == 'user_resize':
+                        if params[2] == 'true':
+                            default_config['user_resize'] = True
                             event.add_return("reply", ["允许用户设置图片大小"])
-                        elif params[2] == "false":
-                            default_config["user_resize"] = False
+                        elif params[2] == 'false':
+                            default_config['user_resize'] = False
                             event.add_return("reply", ["禁止用户设置图片大小"])
-                    if params[1] == "gen_limit":
-                        if params[2] == "true":
-                            default_config["gen_limit"] = True
+                    if params[1] == 'gen_limit':
+                        if params[2] == 'true':
+                            default_config['gen_limit'] = True
                             event.add_return("reply", ["开启生成数量限制"])
-                        elif params[2] == "false":
-                            default_config["gen_limit"] = False
+                        elif params[2] == 'false':
+                            default_config['gen_limit'] = False
                             event.add_return("reply", ["关闭生成数量限制"])
-                    if params[1] == "ntag":
-                        default_config["negative_prompt"] = params[2]
+                    if params[1] == 'ntag':
+                        default_config['negative_prompt'] = params[2]
                         event.add_return("reply", [f"设置默认负面词条为:{params[2]}"])
-                    if params[1] == "model":
-                        default_config["model"] = params[2]
+                    if params[1] == 'model':
+                        default_config['model'] = params[2]
                         event.add_return("reply", [f"设置默认生成模型为:{params[2]}"])
-                    if params[1] == "width":
-                        default_config["width"] = params[2]
+                    if params[1] == 'width':
+                        default_config['width'] = params[2]
                         event.add_return("reply", [f"设置默认宽为:{params[2]}"])
-                    if params[1] == "height":
-                        default_config["height"] = params[2]
+                    if params[1] == 'height':
+                        default_config['height'] = params[2]
                         event.add_return("reply", [f"设置默认高为:{params[2]}"])
-                    if params[1] == "steps":
-                        default_config["steps"] = params[2]
+                    if params[1] == 'steps':
+                        default_config['steps'] = params[2]
                         event.add_return("reply", [f"设置默认步数为:{params[2]}"])
-                    if params[1] == "cfg_scale":
-                        default_config["cfg_scale"] = params[2]
+                    if params[1] == 'cfg_scale':
+                        default_config['cfg_scale'] = params[2]
                         event.add_return("reply", [f"设置默认词条相关度为:{params[2]}"])
-                    if params[1] == "num_images":
-                        default_config["num_images"] = params[2]
+                    if params[1] == 'num_images':
+                        default_config['num_images'] = params[2]
                         event.add_return("reply", [f"设置默认生成数量为:{params[2]}"])
-                    if params[1] == "quality_tags":
-                        default_config["quality_tags"] = params[2]
+                    if params[1] == 'quality_tags':
+                        default_config['quality_tags'] = params[2]
                         event.add_return("reply", [f"设置质量标签状态为:{params[2]}"])
             else:
                 event.add_return("reply", ["你没有权限调用"])
